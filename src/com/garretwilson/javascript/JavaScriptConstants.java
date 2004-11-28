4e66603dcd9b35fe4d975c2e3626f0df104bb316
Garret Wilson <garret@globalmentor.com>
2004-11-28 08:27:14 +0000

    modified JSF button renderer to use JavaScript to get around IE button tag bug

---
+++ src/com/garretwilson/javascript/JavaScriptConstants.java
@@ -0,0 +1,44 @@
+package com.garretwilson.javascript;
+
+/**Constants for working with JavaScript.
+@author Garret Wilson
+*/
+public class JavaScriptConstants
+{
+
+	/**The delimiter '.' for object properties.*/
+	public final static char PROPERTY_DELIMITER='.';
+
+	/**The character '[' for starting an array index.*/
+	public final static char ARRAY_INDEX_BEGIN_CHAR='[';
+
+	/**The character ']' for ending an array index.*/
+	public final static char ARRAY_INDEX_END_CHAR=']';
+
+	/**The character '(' for starting a parameter list.*/
+	public final static char PARAMETER_BEGIN_CHAR='(';
+
+	/**The character ')' for ending a parameter list.*/
+	public final static char PARAMETER_END_CHAR=')';
+
+	/**The delimiter ',' for method parameter separation.*/
+	public final static char PARAMETER_DELIMITER=',';
+
+	/**The character '=' for variable assignment.*/
+	public final static char ASSIGNMENT_CHAR='=';
+
+	/**The character ';' indicating the ending of a statement.*/
+	public final static char STATEMENT_END_CHAR=';';
+
+	/**The document identifier.*/
+	public final static String DOCUMENT="document";
+
+	/**The forms identifier.*/
+	public final static String FORMS="forms";
+
+	/**The form submission method.*/
+	public final static String SUBMIT_METHOD="submit";
+
+	/**The null identifier.*/
+	public final static String NULL="null";
+}
