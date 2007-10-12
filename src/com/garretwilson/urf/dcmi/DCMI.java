e882e17ccb19622a2f70bc58367ad4e41ed3e850
Garret Wilson <garret@globalmentor.com>
2007-10-12 20:46:42 +0000

    Tidied URF source code for publication.

--- src/com/garretwilson/urf/dcmi/DCMI.java
+++ src/com/garretwilson/urf/dcmi/DCMI.java
@@ -7,7 +7,13 @@ import com.garretwilson.urf.*;
 import static com.garretwilson.urf.URF.*;
 import static com.garretwilson.util.LocaleUtilities.*;
 
-/**Constants used for Dublin Core as stored in URF.
+/**Constants and methods used for Dublin Core as stored in URF.
+<p>Copyright © 2007 GlobalMentor, Inc.
+This source code can be freely used for any purpose, as long as the following conditions are met.
+Any object code derived from this source code must include the following text to users using along with other "about" notifications:
+"Uniform Resource Framework (URF) <http://www.urf.name/> specification and processing
+written by Garret Wilson <http://www.garretwilson.com/> and Copyright © 2007 GlobalMentor, Inc. <http://www.globalmentor.com/>."
+Any redistribution of this source code or derived source code must include these comments unmodified.</p>
 @author Garret Wilson
 @see <a href="http://dublincore.org/documents/dcmi-namespace/">DCMI Namespace Policy</a>
 */
