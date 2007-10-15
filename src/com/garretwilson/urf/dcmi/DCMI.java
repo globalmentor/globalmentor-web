f3179fea8e54a6102f946e10e4304f4189fa05a3
Garret Wilson <garret@globalmentor.com>
2007-10-15 23:10:35 +0000

    Fixed various bugs and typos.

--- src/com/garretwilson/urf/dcmi/DCMI.java
+++ src/com/garretwilson/urf/dcmi/DCMI.java
@@ -10,8 +10,8 @@ import static com.garretwilson.urf.URF.*;
 <p>Copyright © 2007 GlobalMentor, Inc.
 This source code can be freely used for any purpose, as long as the following conditions are met.
 Any object code derived from this source code must include the following text to users using along with other "about" notifications:
-"Uniform Resource Framework (URF) <http://www.urf.name/> specification and processing
-written by Garret Wilson <http://www.garretwilson.com/> and Copyright © 2007 GlobalMentor, Inc. <http://www.globalmentor.com/>."
+"Uniform Resource Framework (URF) &lt;http://www.urf.name/&gt; specification and processing
+written by Garret Wilson &lt;http://www.garretwilson.com/&gt; and Copyright © 2007 GlobalMentor, Inc. &lt;http://www.globalmentor.com/&gt;."
 Any redistribution of this source code or derived source code must include these comments unmodified.</p>
 @author Garret Wilson
 @see <a href="http://dublincore.org/documents/dcmi-namespace/">DCMI Namespace Policy</a>
