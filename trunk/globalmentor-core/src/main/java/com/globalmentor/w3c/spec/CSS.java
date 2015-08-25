2223679097316f8cec81ff6bed839220819cecea
Garret Wilson <garret@globalmentor.com>
2015-08-25 21:14:44 +0000

    Moved CSS cursor definition to CSS specification class.

--- trunk/globalmentor-core/src/main/java/com/globalmentor/w3c/spec/CSS.java
+++ trunk/globalmentor-core/src/main/java/com/globalmentor/w3c/spec/CSS.java
@@ -22,6 +22,7 @@ import java.util.regex.Pattern;
 import org.w3c.dom.css.*;
 
 import com.globalmentor.java.Characters;
+import com.globalmentor.lex.Identifier;
 import com.globalmentor.net.ContentType;
 
 import static com.globalmentor.java.Characters.*;
@@ -504,6 +505,52 @@ public class CSS {
 	public static final String CSS_VISIBILITY_HIDDEN = "hidden";
 	public static final String CSS_VISIBILITY_INHERIT = "inherit";
 	public static final String CSS_VISIBILITY_VISIBLE = "visible";
+
+	/**
+	 * Standard cursors supported by CSS.
+	 * @author Garret Wilson
+	 * @see <a href="http://www.w3.org/TR/CSS21/ui.html">CSS 2.1 User Interface: Cursors</a>
+	 */
+	public static enum Cursor implements Identifier {
+
+		/** The UA determines the cursor to display based on the current context. */
+		AUTO,
+		/** A simple crosshair (e.g., short line segments resembling a "+" sign). */
+		CROSSHAIR,
+		/** The platform-dependent default cursor. Often rendered as an arrow. */
+		DEFAULT,
+		/** The cursor is a pointer that indicates a link. */
+		POINTER,
+		/** Indicates something is to be moved. */
+		MOVE,
+		/** Indicate that some edge is to be moved from the east of the box. */
+		E_RESIZE,
+		/** Indicate that some edge is to be moved from the north-east corner of the box. */
+		NE_RESIZE,
+		/** Indicate that some edge is to be moved from the north-west corner of the box. */
+		NW_RESIZE,
+		/** Indicate that some edge is to be moved from the north of the box. */
+		N_RESIZE,
+		/** Indicate that some edge is to be moved from the south-east corner of the box. */
+		SE_RESIZE,
+		/** Indicate that some edge is to be moved from the south-west corner of the box. */
+		SW_RESIZE,
+		/** Indicate that some edge is to be moved from the south corner of the box. */
+		S_RESIZE,
+		/** Indicate that some edge is to be moved from the west of the box. */
+		W_RESIZE,
+		/** Indicates text that may be selected. Often rendered as an I-beam. */
+		TEXT,
+		/** Indicates that the program is busy and the user should wait. Often rendered as a watch or hourglass. */
+		WAIT,
+		/**
+		 * A progress indicator. The program is performing some processing, but is different from {@link #WAIT} in that the user may still interact with the program.
+		 * Often rendered as a spinning beach ball, or an arrow with a watch or hourglass.
+		 */
+		PROGRESS,
+		/** Help is available for the object under the cursor. Often rendered as a question mark or a balloon. */
+		HELP;
+	}
 
 	/**
 	 * Returns a string to display on a marker for the given list item.
