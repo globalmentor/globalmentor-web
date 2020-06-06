e4f923df827665f4ab03c3d16020d32e90e30012
Garret Wilson <garret@globalmentor.com>
2020-06-06 09:44:14 -0700

    JAVA-164: Modified vocabulary definitions to use vocabulary term class.

--- vocab-dcmi/src/main/java/com/globalmentor/vocab/dcmi/DCMES.java
+++ vocab-dcmi/src/main/java/com/globalmentor/vocab/dcmi/DCMES.java
@@ -18,6 +18,8 @@ package com.globalmentor.vocab.dcmi;
 
 import java.net.URI;
 
+import com.globalmentor.vocab.VocabularyTerm;
+
 /**
  * Constants and methods used for Dublin Core Metadata Element Set (DCMES).
  * @apiNote The core Dublin Core Metadata Element Set has been incorporated into the more recent DCMI Metadata Terms.
@@ -37,7 +39,7 @@ public class DCMES {
 	/**
 	 * The URI to the obsolete Dublin Core Metadata Element Set 1.0 namespace.
 	 * <p>
-	 * <em>This namespace is now obsolete.</em>
+	 * <em>This namespace is now obsolete and should not be used.</em>
 	 * </p>
 	 * @apiNote This namespace was used in the Open eBook Publication Structure (OEBPS) 1.1 specification, for example.
 	 */
@@ -57,68 +59,35 @@ public class DCMES {
 	/** The URI to the Dublin Core Metadata Element Set namespace. */
 	public static final URI NAMESPACE_URI = DCMES_1_1_ELEMENTS_NAMESPACE_URI;
 
-	//Dublin Core property names
 	/** The title of a resource. */
-	public static final String TITLE_PROPERTY_NAME = "title";
+	public static final VocabularyTerm TERM_TITLE = VocabularyTerm.of(NAMESPACE_URI, "title");
 	/** The creator of a resource. */
-	public static final String CREATOR_PROPERTY_NAME = "creator";
+	public static final VocabularyTerm TERM_CREATOR = VocabularyTerm.of(NAMESPACE_URI, "creator");
 	/** The subject of a resource. */
-	public static final String SUBJECT_PROPERTY_NAME = "subject";
+	public static final VocabularyTerm TERM_SUBJECT = VocabularyTerm.of(NAMESPACE_URI, "subject");
 	/** The description of a resource. */
-	public static final String DESCRIPTION_PROPERTY_NAME = "description";
+	public static final VocabularyTerm TERM_DESCRIPTION = VocabularyTerm.of(NAMESPACE_URI, "description");
 	/** The publisher of a resource. */
-	public static final String PUBLISHER_PROPERTY_NAME = "publisher";
+	public static final VocabularyTerm TERM_PUBLISHER = VocabularyTerm.of(NAMESPACE_URI, "publisher");
 	/** The contributor of a resource. */
-	public static final String CONTRIBUTOR_PROPERTY_NAME = "contributor";
+	public static final VocabularyTerm TERM_CONTRIBUTOR = VocabularyTerm.of(NAMESPACE_URI, "contributor");
 	/** The date of a resource. */
-	public static final String DATE_PROPERTY_NAME = "date";
+	public static final VocabularyTerm TERM_DATE = VocabularyTerm.of(NAMESPACE_URI, "date");
 	/** The Dublin Core type of a resource. */
-	public static final String TYPE_PROPERTY_NAME = "type";
+	public static final VocabularyTerm TERM_TYPE = VocabularyTerm.of(NAMESPACE_URI, "type");
 	/** The format of a resource. */
-	public static final String FORMAT_PROPERTY_NAME = "format";
+	public static final VocabularyTerm TERM_FORMAT = VocabularyTerm.of(NAMESPACE_URI, "format");
 	/** The Dublin Core identifier of a resource. */
-	public static final String IDENTIFIER_PROPERTY_NAME = "identifier";
+	public static final VocabularyTerm TERM_IDENTIFIER = VocabularyTerm.of(NAMESPACE_URI, "identifier");
 	/** The source of a resource. */
-	public static final String SOURCE_PROPERTY_NAME = "source";
+	public static final VocabularyTerm TERM_SOURCE = VocabularyTerm.of(NAMESPACE_URI, "source");
 	/** The language of a resource. */
-	public static final String LANGUAGE_PROPERTY_NAME = "language";
+	public static final VocabularyTerm TERM_LANGUAGE = VocabularyTerm.of(NAMESPACE_URI, "language");
 	/** The relation of a resource. */
-	public static final String RELATION_PROPERTY_NAME = "relation";
+	public static final VocabularyTerm TERM_RELATION = VocabularyTerm.of(NAMESPACE_URI, "relation");
 	/** The coverage of a resource. */
-	public static final String COVERAGE_PROPERTY_NAME = "coverage";
+	public static final VocabularyTerm TERM_COVERAGE = VocabularyTerm.of(NAMESPACE_URI, "coverage");
 	/** The rights of a resource. */
-	public static final String RIGHTS_PROPERTY_NAME = "rights";
-
-	//Dublin Core property URIs
-	/** The title of a resource. */
-	public static final URI TITLE_PROPERTY_URI = NAMESPACE_URI.resolve(TITLE_PROPERTY_NAME);
-	/** The creator of a resource. */
-	public static final URI CREATOR_PROPERTY_URI = NAMESPACE_URI.resolve(CREATOR_PROPERTY_NAME);
-	/** The subject of a resource. */
-	public static final URI SUBJECT_PROPERTY_URI = NAMESPACE_URI.resolve(SUBJECT_PROPERTY_NAME);
-	/** The description of a resource. */
-	public static final URI DESCRIPTION_PROPERTY_URI = NAMESPACE_URI.resolve(DESCRIPTION_PROPERTY_NAME);
-	/** The publisher of a resource. */
-	public static final URI PUBLISHER_PROPERTY_URI = NAMESPACE_URI.resolve(PUBLISHER_PROPERTY_NAME);
-	/** The contributor of a resource. */
-	public static final URI CONTRIBUTOR_PROPERTY_URI = NAMESPACE_URI.resolve(CONTRIBUTOR_PROPERTY_NAME);
-	/** The date of a resource. */
-	public static final URI DATE_PROPERTY_URI = NAMESPACE_URI.resolve(DATE_PROPERTY_NAME);
-	/** The Dublin Core type of a resource. */
-	public static final URI TYPE_PROPERTY_URI = NAMESPACE_URI.resolve(TYPE_PROPERTY_NAME);
-	/** The format of a resource. */
-	public static final URI FORMAT_PROPERTY_URI = NAMESPACE_URI.resolve(FORMAT_PROPERTY_NAME);
-	/** The Dublin Core identifier of a resource. */
-	public static final URI IDENTIFIER_PROPERTY_URI = NAMESPACE_URI.resolve(IDENTIFIER_PROPERTY_NAME);
-	/** The source of a resource. */
-	public static final URI SOURCE_PROPERTY_URI = NAMESPACE_URI.resolve(SOURCE_PROPERTY_NAME);
-	/** The language of a resource. */
-	public static final URI LANGUAGE_PROPERTY_URI = NAMESPACE_URI.resolve(LANGUAGE_PROPERTY_NAME);
-	/** The relation of a resource. */
-	public static final URI RELATION_PROPERTY_URI = NAMESPACE_URI.resolve(RELATION_PROPERTY_NAME);
-	/** The coverage of a resource. */
-	public static final URI COVERAGE_PROPERTY_URI = NAMESPACE_URI.resolve(COVERAGE_PROPERTY_NAME);
-	/** The rights of a resource. */
-	public static final URI RIGHTS_PROPERTY_URI = NAMESPACE_URI.resolve(RIGHTS_PROPERTY_NAME);
+	public static final VocabularyTerm TERM_RIGHTS = VocabularyTerm.of(NAMESPACE_URI, "rights");
 
 }
--- vocab-foaf/src/main/java/com/globalmentor/vocab/foaf/FOAF.java
+++ vocab-foaf/src/main/java/com/globalmentor/vocab/foaf/FOAF.java
@@ -18,50 +18,53 @@ package com.globalmentor.vocab.foaf;
 
 import java.net.URI;
 
+import com.globalmentor.vocab.VocabularyTerm;
+
 /**
- * Definition of the <a href="http://xmlns.com/foaf/0.1/">FOAF Vocabulary Specification</a>.
+ * Definition of the <a href="http://xmlns.com/foaf/spec/">FOAF Vocabulary Specification</a>.
  * @author Garret Wilson
- * @see <a href="http://xmlns.com/foaf/0.1/">FOAF Vocabulary Specification</a>
+ * @see <a href="http://xmlns.com/foaf/spec/">FOAF Vocabulary Specification</a>
  */
 public class FOAF {
 
 	/** The recommended prefix to the FOAF namespace. */
-	public static final String FOAF_NAMESPACE_PREFIX = "foaf";
+	public static final String NAMESPACE_PREFIX = "foaf";
+
 	/** The URI to the FOAF namespace. */
-	public static final URI FOAF_NAMESPACE_URI = URI.create("http://xmlns.com/foaf/0.1/");
+	public static final URI NAMESPACE_URI = URI.create("http://xmlns.com/foaf/0.1/");
 
 	//FOAF type names
 	/** An agent (e.g. person, group, software or physical artifact). */
-	public static final String AGENT_TYPE_NAME = "Agent";
+	public static final VocabularyTerm TYPE_AGENT = VocabularyTerm.of(NAMESPACE_URI, "Agent");
 	/** A church. */
-	public static final String CHURCH_TYPE_NAME = "Church";
+	public static final VocabularyTerm TYPE_CHURCH = VocabularyTerm.of(NAMESPACE_URI, "Church");
 	/** A company. */
-	public static final String COMPANY_TYPE_NAME = "Company";
+	public static final VocabularyTerm TYPE_COMPANY = VocabularyTerm.of(NAMESPACE_URI, "Company");
 	/** A non-governmental organization. */
-	public static final String NGO_TYPE_NAME = "NGO";
+	public static final VocabularyTerm TYPE_NGO = VocabularyTerm.of(NAMESPACE_URI, "NGO");
 	/** A non-profit organization. */
-	public static final String NONPROFIT_TYPE_NAME = "Nonprofit";
+	public static final VocabularyTerm TYPE_NONPROFIT = VocabularyTerm.of(NAMESPACE_URI, "Nonprofit");
 	/** An organization. */
-	public static final String ORGANIZATION_TYPE_NAME = "Organization";
+	public static final VocabularyTerm TYPE_ORGANIZATION = VocabularyTerm.of(NAMESPACE_URI, "Organization");
 	/** A person. */
-	public static final String PERSON_TYPE_NAME = "Person";
+	public static final VocabularyTerm TYPE_PERSON = VocabularyTerm.of(NAMESPACE_URI, "Person");
 	/** A religious organization. */
-	public static final String RELIGIOUS_ORGANIZATION_TYPE_NAME = "ReligiousOrganization";
+	public static final VocabularyTerm TYPE_RELIGIOUS_ORGANIZATION = VocabularyTerm.of(NAMESPACE_URI, "ReligiousOrganization");
 	/** A school. */
-	public static final String SCHOOL_TYPE_NAME = "School";
+	public static final VocabularyTerm TYPE_SCHOOL = VocabularyTerm.of(NAMESPACE_URI, "School");
 	/** A synagogue. */
-	public static final String SYNAGOGUE_TYPE_NAME = "Synagogue";
+	public static final VocabularyTerm TYPE_SYNAGOGUE = VocabularyTerm.of(NAMESPACE_URI, "Synagogue");
 	/** A temple. */
-	public static final String TEMPLE_TYPE_NAME = "Temple";
+	public static final VocabularyTerm TYPE_TEMPLE = VocabularyTerm.of(NAMESPACE_URI, "Temple");
 	/** A university. */
-	public static final String UNIVERSITY_TYPE_NAME = "University";
+	public static final VocabularyTerm TYPE_UNIVERSITY = VocabularyTerm.of(NAMESPACE_URI, "University");
 
 	//FOAF property names
 	/** The gender of this Agent (typically but not necessarily "male" or "female"). */
-	public static final String GENDER_PROPERTY_NAME = "gender";
+	public static final VocabularyTerm PROPERTY_GENDER = VocabularyTerm.of(NAMESPACE_URI, "gender");
 	/** A personal mailbox. */
-	public static final String MBOX_PROPERTY_NAME = "mbox";
+	public static final VocabularyTerm PROPERTY_MBOX = VocabularyTerm.of(NAMESPACE_URI, "mbox");
 	/** A name for some thing. */
-	public static final String NAME_PROPERTY_NAME = "name";
+	public static final VocabularyTerm PROPERTY_NAME = VocabularyTerm.of(NAMESPACE_URI, "name");
 
 }
